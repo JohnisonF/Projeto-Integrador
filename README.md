a0a814ce08a30c75e5b34f68fbbe287a226983e1
JohnisonF <john-ison@hotmail.com>
2022-11-28 17:38:23 -0300

    Merge

--- README.md
+++ README.md
@@ -10,3 +10,6 @@
 | #3            | FEAT - Adicionando menu topo   |
 | #4            | FIX/FEAT - Corrigindo erros de rota e adicionando boletim  |
 | #5            | FEAT - Criando Biblioteca Digital  |
+| #6            | FIX/FEAT - Resolvendo bug de layout e colocando livros  |
+| #7            | FEAT - Criando a página de disciplinas  |
+| #8            | CHORE - Atulizando a sidebar e botões  |
--- views/view/template/sidebar.php
+++ views/view/template/sidebar.php
@@ -1,27 +1,38 @@
 <div id="sidebar-buttons">
 	<div class="menu">
 		<div class="accordion" id="accordionExample">
-		  <div class="accordion-item d-none">
+		  <div class="accordion-item">
 		    <h2 class="accordion-header" id="headingOne">
 		      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-		        Accordion Item #1
+		        Curso
 		      </button>
 		    </h2>
 		    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
 		      <div class="accordion-body">
-		        TESTINHO
+		        <ul>
+		        	<li>
+		        		<a href="">Disciplinas</a>
+		        	</li>
+		        	<li>
+		        		<a href="">Atividades</a>
+		        	</li>
+		        </ul>
 		      </div>
 		    </div>
 		  </div>
-		  <div class="accordion-item d-none">
+		  <div class="accordion-item">
 		    <h2 class="accordion-header" id="headingTwo">
 		      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-		        Accordion Item #2
+		        Serviços
 		      </button>
 		    </h2>
 		    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
 		      <div class="accordion-body">
-		        toooool
+		        <ul>
+		        	<li>
+		        		<a href="">Solicitação de serviços</a>
+		        	</li>
+		        </ul>
 		      </div>
 		    </div>
 		  </div>
